c726e922b9e79b0d91d2fb77c0a4391502918b3a
Christopher Buquicchio <buquicchioc@gmail.com>
2017-08-16 16:20:19 -0400

    Add setup-workspace script for tmux
    * This script initializes a simple tmux session
      with 3 panes (bash, vim, and another bash to
      run a server/compiler/debugger)

---
+++ setup-workspace.sh
@@ -0,0 +1,12 @@
+if [ $# -ne 0 ]
+then
+    session_name=$1
+    tmux new-session -s ${session_name} -n main -d
+    tmux new-window -n vim -t ${session_name} vim
+    tmux new-window -n server/compiler -t ${session_name}
+    tmux split-window -t ${session_name}:2
+
+    tmux attach-session -t ${session_name}:0
+else
+    echo "Please provide a session name"
+fi
